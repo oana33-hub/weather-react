052994e671ecb5444871492b97b18a6bd094997a
oana33-hub <70967983+oana33-hub@users.noreply.github.com>
2020-10-31 16:17:21 +0100

    changed color of loader to orange

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props){
        
             <Loader
          type="Puff"
-         color="#00BFFF"
+         color="orange"
          height={100}
          width={100}
          timeout={3000} 
