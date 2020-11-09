25f253171efef546baae1d2d08f7e0708fae1f0f
oana33-hub <70967983+oana33-hub@users.noreply.github.com>
2020-11-09 14:58:33 +0100

    implemented the search engine

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Form from "./Form";
-import City from "./City";
-import Date from "./Date";
 import Temperature from "./Temperature";
-import Description from "./Description";
+
 import Daily from "./Daily";
 import Weekdays from "./Weekdays";
 import Footer from "./Footer";
 import "./footer.css";
 import "./weekdays.css";
 import "./styles.css";
-import "./city.css";
-import "./date.css";
+
+
 import "./form.css";
 import "./temperature.css";
-import "./description.css";
+
 import "./daily.css";
 
 export default function App() {
@@ -24,10 +22,8 @@ export default function App() {
         <div className="weather-app-wrapper">
           <div className="card-body">
             <Form />
-            <City />
-            <Date />
             <Temperature />
-            <Description />
+           
             <Daily />
             <Weekdays />
             <Footer />
--- src/Form.js
+++ src/Form.js
@@ -1,37 +1,50 @@
-import React from "react";
-
+ import React, { useState } from "react";
+import axios from "axios";
+import "./form.css";
 export default function Form() {
+  let [city, setCity] = useState("");
+  let [response, setResponse] = useState("");
+
+  function showTemperature(response) {
+    if (city === "") {
+      setResponse(`Searching for ${city}`);
+    } else {
+      setResponse(
+       <div>
+          <h1 class="city"> {city}</h1>
+          <h2>Temperature: {Math.round(response.data.main.temp)}°C </h2>
+          <ul>
+          <li>Description: {response.data.weather[0].main} </li>
+          <li>Humidity: {Math.round(response.data.main.humidity)}% </li>
+          <li>Wind Speed: {response.data.wind.speed} km/h </li>
+          <li>
+            <img
+              src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              alt=""
+            ></img>
+          </li>
+        </ul>
+        </div>
+      );
+    }
+  }
+  function getData(event) {
+    event.preventDefault();
+    let apiKey = "71d6dfd3b336163612f9cfac0fa5a0ed";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+  function City(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div>
-      <form id="search-form" />
-      <div>
-        <input
-          type="search"
-          placeholder="Search a city"
-          autofocus="on"
-          autocomplete="off"
-          id="city-input"
-          className="search-form"
-        />
-      </div>
-
-      <input
-        type="submit"
-        value="search"
-        className="btn btn-info "
-        id="search-city"
-      />
-
-      <button
-        type="button"
-        className="btn btn-info"
-        id="current-location-button"
-      >
-        Current
-      </button>
+      <form>
+        <input type="search" onChange={City} placeholder="Search for a city" autoCapitalize="on" autoComplete="off"autoFocus="on"/>
+        <input type="submit" value="search" onClick={getData} />
+      </form>
+      <h1>{response}</h1>
     </div>
   );
 }
-
-       
-    
